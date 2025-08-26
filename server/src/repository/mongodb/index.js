e9c89680adf8ae084d6bd6019c237c2b107edc4f
Andrei Krivenko <confleux@gmail.com>
2025-08-26 16:46:50 +0300

    chore: add indexes

--- server/src/repository/mongodb/index.js
+++ server/src/repository/mongodb/index.js
@@ -8,6 +8,9 @@ const client = new MongoClient(uri);
 await client.connect();
 const db = client.db(dbName);
 
+await db.collection('blocks').createIndex({ id: 1 }, { unique: true });
+await db.collection('voters').createIndex({ address: 1 }, { unique: true });
+
 export default {
     blocksCollection: db.collection('blocks'),
     transactionsCollection: db.collection('transactions'),
